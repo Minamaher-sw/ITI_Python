ded4e20e24c80d27b741663f8b0e2df455b3be56
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-18 20:11:35 +0200

    Add How  String write and Multi Line String Way

--- Day_1/Main.py
+++ Day_1/Main.py
@@ -5,8 +5,20 @@ print("Hellow World ")
 #Line Indentations in python Level_1 level_2  
 a = 4 ;
 if a == 4 :
-    print("Hello World ")
+    print("Hello World")
 else :
     print("Hello World ")
-
+# Quotes Tricks 
+""" 
+Single Quote is same Double Quotes in use 
+Double Quotes
+Triple Quotes is more readale Double Quote \n in Multi line string 
+"""
+word = ' word' #Single Quote is same Double Quotes in use 
+sentence = "This is a sentence."
+#Triple Quotes is more readale 
+paragraph_1 ="his is a paragraph \n It is made up of multiple lines and sentences \n"
+paragraph_2 = """This is a paragraph.
+ It is made up of multiple lines and sentences.""" 
+print(word ,'\n',sentence,'\n',paragraph_1,paragraph_2)
 
