51f26e25ecbf8cbeacec20265fff887dcb4848b6
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-18 20:01:05 +0200

    Line Identations

--- Day_1/Main.py
+++ Day_1/Main.py
@@ -1,3 +1,12 @@
 # Tricks In Python
 # Hello World Programe 
 print("Hellow World ") 
+
+#Line Indentations in python Level_1 level_2  
+a = 4 ;
+if a == 4 :
+    print("Hello World ")
+else :
+    print("Hello World ")
+
+
