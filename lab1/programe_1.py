16bbac317ff5db43ab61a9a33ff8859cfce9d3dc
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-22 10:27:31 +0200

    adding comment in code

--- lab1/programe_1.py
+++ lab1/programe_1.py
@@ -1,3 +1,8 @@
+
+"""
+program that counts up the number of vowels [a, e, i, o, u] 
+contained in the string 
+"""
 input_string =input("Enter  string ")
 vowels_char =['a', 'e', 'i', 'o', 'u']
 count =0 
