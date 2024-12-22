df681d040f207b4d0ca52e5c7e9475f332e38d7b
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-22 11:45:45 +0200

    adding assending function

--- lab1/programe_3.py
+++ lab1/programe_3.py
@@ -13,7 +13,24 @@ def desending_array(my_array):
                my_array[index] = min_val
 
 desending_array(my_array) 
-print(my_array)            
+print(my_array) 
+
+my_array=[]
+print("Enter arrray for asending way\n")
+for index in range(0,5):
+    my_array.append(int(input(f"enter value of index {index} ")))
+
+def asending_array(my_array):
+   for index in range(0,5) :
+       max_val=my_array[index]
+       for index_2 in range(index ,5):
+           if(my_array[index_2] >= max_val):
+               max_val =my_array[index_2]
+               my_array[index_2] = my_array[index]
+               my_array[index] = max_val
+
+asending_array(my_array) 
+print(my_array) 
 
 
                
