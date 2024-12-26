62d62ad87bed892955368c314b74b33940aea80e
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-26 09:18:20 +0200

    adding to len function and how check string

---
+++ string_func/string_unc.py
@@ -0,0 +1,14 @@
+
+first_name ="Mina Maher MOSadef"
+second_name ="Astifanous Shenouda Elmaraghy"
+digit_str = "1234566788"
+empty_string =""
+#string built-in functions.
+#len lenght function 
+print("lenght of first_name is",len(first_name))
+print("lenght of empty string is" ,len(empty_string))
+
+#check string 
+if "Maher" in first_name :
+    print(True)
+
