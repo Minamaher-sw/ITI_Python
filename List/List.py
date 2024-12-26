b32ca959e376822aca901bc1e38a0ee5dadb47d1
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-26 12:23:19 +0200

    adding use of len function

---
+++ List/List.py
@@ -0,0 +1,13 @@
+"""
+Lists are used to store multiple items in a single variable.
+
+Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+
+Lists are created using square brackets:
+"""
+"""
+    *******************  List Length **************
+To determine how many items a list has, use the len() function:
+"""
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
