89153c18b754b968e506b41ce514b74f53ab4e75
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-26 18:24:11 +0200

    adding how list copy by using list function , copy fnction and sice  way

--- List/List.py
+++ List/List.py
@@ -363,3 +363,32 @@ The reverse() method reverses the current sorting order of the elements.
 thislist = ["banana", "Orange", "Kiwi", "cherry"]
 thislist.reverse()
 print(thislist)
+"""
+   ****************  Python - Copy Lists  ****************
+   Copy a List
+You cannot copy a list simply by typing list2 = list1, because: list2 will only be a reference to list1, 
+and changes made in list1 will automatically also be made in list2.
+Use the copy() method
+
+"""
+#way 1 by use copy function
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+#way 2
+"""
+Use the slice Operator
+You can also make a copy of a list by using the : (slice) operator.
+"""
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+
+
+"""
+    Use the list() method
+Another way to make a copy is to use the built-in method list().
+"""
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
