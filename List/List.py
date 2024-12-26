f8ac39e21e51e9dccf2a4d9ac9505678530c8d80
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-26 15:14:01 +0200

    adding use of Using List Comprehension ways

--- List/List.py
+++ List/List.py
@@ -222,3 +222,83 @@ print(thislist)
 thislist.clear()
 print(thislist)
 
+"""
+   ****************  Python - Loop Lists  *************
+"""
+print("********** Python - Loop Lists ***************\n")
+thislist = ["apple", "banana", "cherry"]
+for item in thislist:
+    print(item)
+
+"""
+    Loop Through the Index Numbers
+You can also loop through the list items by referring to their index number.
+
+Use the range() and len() functions to create a suitable iterable.Loop Through the Index Numbers
+You can also loop through the list items by referring to their index number.
+
+Use the range() and len() functions to create a suitable iterable.
+"""
+print("printing from index by for loop")
+for index in range(len(thislist)):
+    print(thislist[index])
+
+#using while loop
+print("printing from index by while loop")
+index =0 
+while True :
+    print(thislist[index])
+    index+=1
+    if(index == len(thislist)):
+        break 
+"""
+Looping Using List Comprehension
+List Comprehension offers the shortest syntax for looping through lists:
+
+Example
+A short hand for loop that will print all items in a list:
+
+List Comprehension
+List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list.
+
+Example:
+
+Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
+
+Without list comprehension you will have to write a for statement with a conditional test inside:
+
+"""
+print("printing list byUsing List Comprehension ")
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+print(newlist)
+print("printing list after using List Comprehension ")
+"""
+    The Syntax
+newlist = [expression for item in iterable if condition == True]
+The return value is a new list, leaving the old list unchanged.
+"""
+new_list = [x for x in fruits if "a" in x]
+print(newlist)
+
+new_list = [x for x in fruits if x !="apple"]
+print(new_list)
+
+new_list =[x for x in range(10)]
+print(new_list)
+
+new_list =[x.upper() for x in fruits if x !="mango"]
+print(new_list)
+
+new_list =["hello" for x in fruits if x !="mango"]
+print(new_list)
+new_list =[x if x !="banana" else "orange " for x in fruits ]
+print(new_list)
