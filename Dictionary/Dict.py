b6d30fb24828fa40a4d6b8bb80d982fdd8524c13
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-29 01:53:12 +0200

    how access element inside tuple inside list

--- Dictionary/Dict.py
+++ Dictionary/Dict.py
@@ -3,3 +3,7 @@
 t1 =(12 , "mina")
 t2 =(13 ,"maher")
 list_1 =[t1 ,t2]
+print("t1" ,t1 ,"  ",'t2' ,t2)
+print("list_1 =[t1 ,t2] is :",list_1)
+print("to get first ellemnet in t1 is 12")
+print("fisrt elemet it t1 list_1[0][0] is ",list_1[0][0])
