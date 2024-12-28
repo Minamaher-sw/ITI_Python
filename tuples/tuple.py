423b8b0b109e867253108185bdd34f7fc10ac6bd
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-28 21:27:37 +0200

    adding using tuple method count and index

--- tuples/tuple.py
+++ tuples/tuple.py
@@ -181,3 +181,16 @@ If you want to multiply the content of a tuple a given number of times, you can 
 fruits = ("apple", "banana", "cherry")
 mytuple = fruits * 2
 print(mytuple)
+"""
+    Python - Tuple Methods
+Tuple Methods
+Python has two built-in methods that you can use on tuples.
+
+Method	Description
+count()	Returns the number of times a specified value occurs in a tuple
+index()	Searches the tuple for a specified value and returns the position of where it was found
+"""
+fruits = ("apple", "banana", "cherry" ,"apple")
+print(fruits)
+print("fruits count(apple) :",fruits.count("apple"))
+print("fruits index(apple)" ,fruits.index("apple"))
