b238b1b5258fdfe5756c0377ef245869fea26094
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-29 21:57:56 +0200

    adding using of is and ==

--- tricks_python/main.py
+++ tricks_python/main.py
@@ -1,5 +1,5 @@
 #short if else 
-degree =int(input("enter degree : "))
+degree =80
 if degree < 60 :
     grade ="fair"
 else :
@@ -34,3 +34,28 @@ print("after  tuple way x y" ,x,y)
 # end of print end ="\n" by default
 print("hi mina" ,end=" .")
 print("com")
+
+"""
+    diff bet   is vs ==
+
+    is is address of memory  in same position
+    == values
+"""
+print("string with == and is")
+str_1 ="mina"
+str_2 ="mina"
+if(str_1 == str_2):
+    print("ok")
+
+if(str_1 is str_2):
+    print("ok")
+print("list with == and is")
+lis_1 =[1,2,3]
+lis_2 =[1,2,3]
+if(lis_1 == lis_2):
+    print("ok")
+
+if(lis_1 is lis_2):
+    print("ok")
+else :
+    print("not")
