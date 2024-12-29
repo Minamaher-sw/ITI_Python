5a38ab1a769b63d387b9ed4715e9b73464c25ce7
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-29 20:29:49 +0200

    using short return and or way

--- function/main.py
+++ function/main.py
@@ -1,1 +1,15 @@
 # DRY DON'T Repeet yourself 
+def my_calTax_1(salary):
+    Tax = (20/100) *salary
+    if Tax :
+        return Tax/2
+    return 10
+print(my_calTax_1(3000))
+print(my_calTax_1(0))
+
+#another short wayy
+def my_calTax_2 (salary):
+    Tax = (20/100) *salary
+    return Tax and Tax/2 or 10
+print(my_calTax_2(3000))
+print(my_calTax_2(0))
