96b08f9882f2e9e7a04ebdbe6a4b29eda7451e00
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-29 21:47:47 +0200

    adding two way of swap

--- tricks_python/main.py
+++ tricks_python/main.py
@@ -8,3 +8,25 @@ print (grade )
 
 grade ='fair' if degree < 60 else 'good'
 print(grade)
+
+"""
+    **************** swap way ***************
+    swap two numbers 
+"""
+x =60
+y=70
+#way 1 
+print("x y" ,x,y)
+
+z= x 
+x=y 
+y=z
+print("after swap x y" ,x,y)
+
+#way 2 by use tuple
+x =30
+y=89
+
+print("x y" ,x,y)
+x,y =y,x
+print("after  tuple way x y" ,x,y)
